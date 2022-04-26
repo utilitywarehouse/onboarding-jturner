caa9020cf37f0aa50f35737026d34797f63e1f25
Jake <jake@thinkpad.home>
2022-04-26 16:32:51 +0100

    Minor refactoring. Fixed issue where entering nothing would cause a crash

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "../index.css";
+
+// services
 import { usePokemonService } from "../services/pokemon-service";
 
 const Pokedex = () => {
+    // hooks
     const [pokemon, setPokemon] = useState({
         name: "",
         height: null,
@@ -13,52 +16,60 @@ const Pokedex = () => {
 
     const pokemonService = usePokemonService();
 
+    // functions
     const searchPokemon = async () => {
-        setPokemon(
-            await pokemonService.getPokemon(searchName.toLocaleLowerCase())
-        );
+        if (searchName) {
+            setPokemon(
+                await pokemonService.getPokemon(searchName.toLocaleLowerCase())
+            );
+        }
     };
 
     return (
         <div className="pokedex">
             <div className="pokedex-depth" />
-            <div className="forehead-rectangle"></div>
-            <div className="pokedex-forehead">
-                <div className="light-container">
-                    <div className="large-light" />
-                    <div
-                        className="small-light"
-                        style={{ backgroundColor: "red" }}
-                    />
-                    <div
-                        className="small-light"
-                        style={{ backgroundColor: "yellow" }}
-                    />
-                    <div
-                        className="small-light"
-                        style={{ backgroundColor: "green" }}
-                    />
-                </div>
+            <div className="light-container">
+                <div className="large-light" />
+                <div
+                    className="small-light"
+                    style={{ backgroundColor: "red" }}
+                />
+                <div
+                    className="small-light"
+                    style={{ backgroundColor: "yellow" }}
+                />
+                <div
+                    className="small-light"
+                    style={{ backgroundColor: "green" }}
+                />
             </div>
             <div className="body">
                 <div className="screen">
-                    <img
-                        className="pokemon-img"
-                        src={
-                            pokemon.sprites.front_default
-                                ? pokemon.sprites.front_default
-                                : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
-                        }
-                        alt="pokemon"
-                    />
+                    <div className="img-container">
+                        <img
+                            className="pokemon-img"
+                            src={
+                                pokemon.sprites.front_default
+                                    ? pokemon.sprites.front_default
+                                    : "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"
+                            }
+                            alt="pokemon"
+                        />
+                    </div>
                 </div>
                 <div className="controls">
                     <input
+                        style={{ height: "30px" }}
                         type="text"
-                        placeholder="Enter a Pokemon name"
+                        placeholder="Enter a Pokemon name or number"
                         onChange={(e) => setSearchName(e.target.value)}
                     />
-                    <button type="submit" name="button" onClick={searchPokemon}>
+                    <button
+                        style={{ height: "30px" }}
+                        type="submit"
+                        name="button"
+                        onClick={searchPokemon}
+                    >
                         Search
                     </button>
                     <div className="pokemon-stats">
